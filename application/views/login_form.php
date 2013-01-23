c082a3749738069a9c7076469cd17e4a456a01a7
Arun Lakra <arun.lakra15@gmail.com>
2013-01-23 18:47:53 +0530

    added interface for login and signup pages

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,18 +1,26 @@
 <?php
-	$username = array('type' => 'text','name' => 'username','placeholder' => 'Username');
-	$password = array('type' => 'password','name' => 'password','placeholder' => 'Password');
-	$submit = array('type' => 'submit', 'name' => 'submit', 'content' => 'Sign in', 'class' => 'btn btn-primary pull-right');
+// php variables to be used in Sign in form
+$username = array('type' => 'text','name' => 'username','placeholder' => 'Username', 'autofocus' => 'autofocus', 'class' => 'span4', 'required' => 'required');
+$password = array('type' => 'password','name' => 'password','placeholder' => 'Password', 'class' => 'span4', 'required' => 'required');
+$submitSignIn = array('type' => 'submit', 'name' => 'submit', 'content' => 'Sign in', 'class' => 'btn btn-primary btn-block');
 ?>
 
-<div class="span3 middle">
-	<h2>Sign in</h2>
-	<?php echo form_open('login/validate_credentials'); ?>
-		<label>Username</label>
-		<?php echo form_input($username); ?>
-		<label>Password</label>
-		<?php echo form_password($password); ?>
-		<?php echo anchor('login/signup', 'Create Account', array('class' => 'btn btn-danger')); ?>
-		<?php echo form_button($submit); ?>
-		<div class="clearfix"></div>
-	<?php echo form_close(); ?>
+<div class="container">
+	<div class="row">
+		<div class="span4 offset4 well">
+			<legend>Please Sign In</legend>
+          	<!-- <div class="alert alert-error">
+                <a class="close" data-dismiss="alert" href="#">×</a>Incorrect Username or Password!
+            </div> -->
+            <?php echo form_open('login/validate_credentials'); ?>
+            <?php echo form_input($username); ?>
+            <?php echo form_password($password); ?>
+            <label class="checkbox">
+            	<input type="checkbox" name="remember" value="1"> Remember Me
+            </label>
+            <?php echo form_button($submitSignIn); ?>
+            <?php echo anchor('login/signup', 'Create Account', array('class' => 'btn btn-warning btn-block')); ?>
+            <?php echo form_close(); ?>
+        </div>
+    </div>
 </div>
--- application/views/signup_form.php
+++ application/views/signup_form.php
@@ -1,37 +1,31 @@
-<?php
-	// echo form_open('login/create_member');
-	// echo form_input('first_name', set_value('first_name', 'First Name'));
-	// echo form_input('last_name', set_value('last_name', 'Last Name'));
-	// echo form_input('email_address', set_value('email_address', 'Email Address'));
+<?php  
+// php variables to be used in Create Account form (username has been shared between both forms)
+$first_name = array('type' => 'text','name' => 'first_name','placeholder' => 'First Name', 'required' => 'required', 'autofocus' => 'autofocus', 'class' => 'span4');
+$last_name = array('type' => 'text','name' => 'last_name','placeholder' => 'Last Name', 'required' => 'required', 'class' => 'span4');
+$username = array('type' => 'text','name' => 'username','placeholder' => 'Username', 'autofocus' => 'autofocus', 'class' => 'span4');
+$email_address = array('type' => 'email','name' => 'email_address','placeholder' => 'Your email', 'required' => 'required', 'class' => 'span4');
+$password = array('type' => 'password','name' => 'password','placeholder' => 'Password', 'required' => 'required', 'class' => 'span4');
+$password2 = array('type' => 'password','name' => 'password2','placeholder' => 'Retype Password', 'required' => 'required', 'class' => 'span4');
+$submitSignUp = array('type' => 'submit', 'name' => 'submit', 'content' => 'Sign up now !', 'class' => 'btn btn-warning btn-block');
 ?>
 
-<?php
-	// echo form_input('username', set_value('username', 'Username'));
-	// echo form_input('password', set_value('password', 'Password'));
-	// echo form_input('password2', 'Password Confirm');
-	// echo form_submit('submit', 'Create Acccount');
-	// echo form_close();
-	// echo validation_errors('<p class="error">');
-	$submit = array('type' => 'submit', 'name' => 'submit', 'content' => 'Sign up', 'class' => 'btn btn-primary pull-right');
-?>
-
-<div class="span3 middle">
-	<h2>Sign Up</h2>
-	<?php echo form_open('login/create_member'); ?>
-		<label>First Name</label>
-		<?php echo form_input('first_name', 'First Name'); ?>
-		<label>Last Name</label>
-		<?php echo form_input('last_name', 'Last Name'); ?>
-		<label>Email Address</label>
-		<?php echo form_input('email_address', 'Email Address'); ?>
-		<label>Username</label>
-		<?php echo form_input('username', 'Username'); ?>
-		<label>Password</label>
-		<?php echo form_input('password', 'Password'); ?>
-		<label>Retype Password</label>
-		<?php echo form_input('password2', 'Retype Password'); ?>
-		<?php echo anchor('login', 'Sign in instead', array('class' => 'btn btn-danger')); ?>
-		<?php echo form_button($submit); ?>
-		<div class="clearfix"></div>
-	<?php echo form_close(); ?>
+<div class="container">
+	<div class="row">
+		<div class="span4 offset4 well">
+			<legend>Create Your Account</legend>
+          	<!-- <div class="alert alert-error">
+                <a class="close" data-dismiss="alert" href="#">×</a>Incorrect Username or Password!
+            </div> -->
+            <?php echo form_open('login/create_member'); ?>
+            <?php echo form_input($first_name); ?>
+            <?php echo form_input($last_name); ?>
+            <?php echo form_input($username); ?>
+            <?php echo form_input($email_address); ?>
+            <?php echo form_password($password); ?>
+            <?php echo form_password($password2); ?>
+            <?php echo form_button($submitSignUp); ?>
+            <?php echo anchor('login', 'Sign in instead', array('class' => 'btn btn-primary btn-block')); ?>
+            <?php echo form_close(); ?>
+        </div>
+    </div>
 </div>
